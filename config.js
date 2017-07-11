26dee3752ae43466614c708ee8eada8db515be6a
Ryley Hodgson <rythebreadmaster@gmail.com>
2017-07-11 17:46:41 -0400

    Ready to deploy

--- config.js
+++ config.js
@@ -7,18 +7,18 @@ module.exports = {
 	// The bot's command prefix. The bot will recognize as command any message that begins with it.
 	// i.e: "-schedulebot foo" will trigger the command "foo",
 	//      whereas "ScheduleBot foo" will do nothing at all.
-	prefix:  "-schedulebot", // Tip: If you use "<@YOUR_BOT_USER_ID>", you can have the prefix
+	prefix:  "<@334444477777313796>", // Tip: If you use "<@YOUR_BOT_USER_ID>", you can have the prefix
 	                         // be a mention to your bot.
 
 	// Admin app settings
 	admin_app: {
 		desc: "ScheduleBot admin commands",
-		prefix: "schedulebot-admin"
+		prefix: "kotobot-admin"
 	},
 
 	// The master channel
 	// The bot will announce the events to this channel. It won't listen to other channels.
-	master_channel: "YOUR_MASTER_CHANNEL",
+	master_channel: "334445745316954112",
 
 	// Events are considered "happening" for a margin of time, where users can see that the event
 	// is happening right now. During that time, the event is not considered expired yet.+
@@ -30,7 +30,7 @@ module.exports = {
 	update_interval: 60000, // In milliseconds
 
 	// List of accepted timezones: https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
-	default_timezone: "Europe/Madrid",
+	default_timezone: "America/Chicago",
 
 	// If this option is enabled, the bot will delete the message that triggered it, and its own
 	// response, after the specified amount of time has passed.
@@ -39,15 +39,15 @@ module.exports = {
 	// MANAGE_MESSAGES - 	0x00002000
 	// More info: https://discordapp.com/developers/docs/topics/permissions
 	delete_after_reply: {
-		enabled: true,
+		enabled: false,
 		time: 60000, // In milliseconds
 	},
 
 	// If true, it will delete any message that is not a command from the master channel.
 	// Leave this on to keep your master channel tidy.
 	// This also requires the "manage messages" permission
-	disallow_talking: true
+	disallow_talking: false
 };
 
 // "Add to server" link:
-// https://discordapp.com/oauth2/authorize?client_id=YOUR_CLIENT_ID&scope=bot&permissions=0x00002000
+// https://discordapp.com/oauth2/authorize?client_id=334444477777313796&scope=bot&permissions=0x00002000
